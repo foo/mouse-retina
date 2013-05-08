615f27c826e84fd5748119ad976a7c4580028c9d
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-08 18:48:31 +0200

    Cubes client uses load-cubes.

---
+++ src/cubes-client/cubes-client.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "../load-cubes/load-cubes.hpp"
+
+int main(int argc, char* argv[])
+{
+  std::string cube_path = "../images/e1088_mag1_small/x0005/y0007/z0005/070317_e1088_x0005_y0007_z0005.raw";
+  cube c(cube_path);
+  
+  return 0;
+}
+
