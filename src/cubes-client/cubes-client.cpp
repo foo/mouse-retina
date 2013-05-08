0a2670bef0601c3a598657e5e8a369a2abfab81c
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-08 21:22:51 +0200

    Change test filename.

--- src/cubes-client/cubes-client.cpp
+++ src/cubes-client/cubes-client.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-  char cube_path[] = "../images/e1088_mag1_small/x0005/y0007/z0005/070317_e1088_x0005_y0007_z0005.raw";
+  char cube_path[] = "../images/e1088_mag1_small/x0005/y0007/z0007/070317_e1088_x0005_y0007_z0007.raw";
   cube c(cube_path);
     
   return 0;
