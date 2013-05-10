5750f33b8aec5eda0d3cdc12cca585c37ee32b69
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-10 16:30:13 +0200

    Populating map of cubes.

--- src/load-cubes/dataset.cpp
+++ src/load-cubes/dataset.cpp
@@ -2,6 +2,27 @@
 
 dataset::dataset(const char* const dataset_dir)
 {
+  populate_cube_files(dataset_dir);
+  print_cube_files();
+
+  // todo: create a map from coord to files
+  // todo: find min and max x,y,z and store in dataset class member
+  // todo: check if cube has holes inside
+}
+
+void dataset::print_cube_files()
+{
+  namespace fs = boost::filesystem;
+  for(std::map<coord, fs::path>::iterator i = cube_files.begin();
+      i != cube_files.end(); ++i)
+  {
+    std::cerr << i->first.x << " " << i->first.y << " " << i->first.z
+	      << ": " << i->second << std::endl;
+  }
+}
+
+void dataset::populate_cube_files(const char* const dataset_dir)
+{
   namespace fs = boost::filesystem;
   fs::path dataset_root(dataset_dir);
 
@@ -17,22 +38,25 @@ dataset::dataset(const char* const dataset_dir)
       // <dataset name>_x<4 digits>_y<4 digits>_z<4 digits>.raw
 
       const int x_pos = filename.size()
-	- 4 // extension and dot
-	- 4 // z number
-	- 2 // _z
-	- 4 // y number
-	- 2 //_y
+	- 4  // extension and dot
+	- 4  // z number
+	- 2  // "_z"
+	- 4  // y number
+	- 2  // "_y"
 	- 4; // x number
       
       std::istringstream iss(std::string(filename.begin() + x_pos, filename.end()));
 
-      int x, y, z;
-      char c;
-      iss >> x >> c >> c >> y >> c >> c >> z;
-      std::cerr << x << ' ' << y << ' ' << z << '\n';
+      coord c;
+      iss >> c.x;
+      iss.ignore(1, '_');
+      iss.ignore(1, 'y');
+      iss >> c.y;
+      iss.ignore(1, '_');
+      iss.ignore(1, 'z');
+      iss >> c.z;
+
+      cube_files.insert(std::make_pair(c, (*file_itr).path()));
     }
   }
-
-  // todo: create a map from coord to files
-  // todo: find min and max x,y,z and store in dataset class member
 }
--- src/load-cubes/dataset.hpp
+++ src/load-cubes/dataset.hpp
@@ -2,12 +2,17 @@
 
 #include <iostream>
 #include <sstream>
+#include <map>
 #include <boost/filesystem.hpp>
 
 #include "../load-cubes/coord.hpp"
 
 class dataset
 {
+private:
+  void populate_cube_files(const char* const dataset_dir);
+  void print_cube_files();
 public:
+  std::map<coord, boost::filesystem::path> cube_files;
   dataset(const char* const dataset_dir);
 };
