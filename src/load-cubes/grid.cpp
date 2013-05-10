b6a8f861583c70c91ebc29501542473f34fb20be
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-10 09:38:55 +0200

    New module grid.

---
+++ src/load-cubes/grid.cpp
@@ -0,0 +1,6 @@
+#include "grid.hpp"
+
+grid::grid(const char* const dataset)
+{
+  std::cerr << "Loading grid from dataset." << std::endl;
+}
---
+++ src/load-cubes/grid.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+
+#include "cube.hpp"
+
+class grid
+{
+public:
+grid(const char* const dataset);
+};
