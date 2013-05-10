861c010da98117c3157dc71b595d90124703871c
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-10 15:16:15 +0200

    Listing directory using Boost.

--- src/load-cubes/dataset.cpp
+++ src/load-cubes/dataset.cpp
@@ -4,4 +4,10 @@ dataset::dataset(const char* const dataset_dir)
 {
   namespace fs = boost::filesystem;
   fs::path dataset_root(dataset_dir);
+
+  fs::recursive_directory_iterator end;
+  fs::recursive_directory_iterator dir(dataset_dir);
+  for(; dir != end; ++dir) {
+    std::cerr << *dir << std::endl;                                    
+  }
 }
--- src/load-cubes/dataset.hpp
+++ src/load-cubes/dataset.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <boost/filesystem.hpp>
 
 class dataset
