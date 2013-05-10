3e2b085b05743244ccf1ab88c598fa65d8c0bdaa
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-10 09:39:13 +0200

    Client code makes use of grid module.

--- src/cubes-client/cubes-client.cpp
+++ src/cubes-client/cubes-client.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
-#include "../load-cubes/load-cubes.hpp"
+#include "../load-cubes/cube.hpp"
+#include "../load-cubes/grid.hpp"
 
 int main(int argc, char* argv[])
 {
   char cube_path[] = "../images/e1088_mag1_small/x0005/y0007/z0007/070317_e1088_x0005_y0007_z0007.raw";
   cube c(cube_path);
-    
+
+  grid g("../images/e1088_mag1_small");
   return 0;
 }
 
