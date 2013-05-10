1f03411f91754345a06192722a90862c5a7ce4cd
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-10 18:08:32 +0200

    Cache mechanism for cubes done.

--- src/load-cubes/grid.cpp
+++ src/load-cubes/grid.cpp
@@ -1,15 +1,42 @@
 #include "grid.hpp"
 
-grid::grid(const dataset& ds)
-{
-  std::string cube_path = (++ds.cube_files.begin())->second.string();
-
-  cube c(cube_path.c_str());
-  cube_cache.insert(std::make_pair(coord(1, 1, 1), std::move(c)));
-}
-
 int grid::cube_cache_size() const
 {
   return cube_cache.size();
 }
 
+void grid::unload_from_cache(const coord& c)
+{
+  cube_cache.erase(cube_cache.find(c));
+}
+
+const cube& grid::get(const coord& c)
+{
+  std::map<coord, cube>::iterator search_for_cube;
+  search_for_cube = cube_cache.find(c);
+
+  if(search_for_cube != cube_cache.end())
+  {
+    return search_for_cube->second;
+  }
+  else
+  {
+    // load cube from disk to memory
+
+    auto search_for_file = ds.cube_files.find(c);
+
+    if(search_for_file != ds.cube_files.end())
+    {
+      cube new_cube(search_for_file->second.string().c_str());
+      cube_cache.insert(std::make_pair(c, std::move(new_cube)));
+      return cube_cache.find(c)->second; // cannot return new_cube, as it is ripped out of its internals
+    }
+    else
+    {
+      std::cerr
+	<< "Cube out of dataset. Program tried to cache cube "
+	<< c.x << ' ' << c.y << ' ' << c.z << std::endl;
+      exit(1);
+    }
+  }
+}
--- src/load-cubes/grid.hpp
+++ src/load-cubes/grid.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <boost/filesystem.hpp>
 
 #include "cube.hpp"
 #include "coord.hpp"
@@ -11,8 +12,11 @@
 class grid
 {
 private:
+  const dataset& ds;
   std::map<coord, cube> cube_cache;
 public:
-  grid(const dataset& ds);
+  grid(const dataset& ds) : ds(ds) {}
   int cube_cache_size() const;
+  const cube& get(const coord& c);
+  void unload_from_cache(const coord& c);
 };
