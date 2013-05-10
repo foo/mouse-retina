ea4ac939a810cffa38c2c0e38157b448f2e32cfc
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-10 12:01:00 +0200

    Make use of move semantics.

--- src/load-cubes/grid.cpp
+++ src/load-cubes/grid.cpp
@@ -7,7 +7,7 @@ grid::grid(const char* const dataset)
   std::string cube_path = std::string(dataset) + "/x0005/y0007/z0007/070317_e1088_x0005_y0007_z0007.raw";
 
   cube c(cube_path.c_str());
-  cube_cache.insert(std::make_pair(coord(1, 1, 1), c));
+  cube_cache.insert(std::make_pair(coord(1, 1, 1), std::move(c)));
 }
 
 int grid::cube_cache_size() const
