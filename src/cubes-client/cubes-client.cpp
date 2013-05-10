eafec167cbbd300a3f4fd64ceb8281a1c634de85
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-10 09:59:35 +0200

    Cube client uses grid cache size query.

--- src/cubes-client/cubes-client.cpp
+++ src/cubes-client/cubes-client.cpp
@@ -5,10 +5,9 @@
 
 int main(int argc, char* argv[])
 {
-  char cube_path[] = "../images/e1088_mag1_small/x0005/y0007/z0007/070317_e1088_x0005_y0007_z0007.raw";
-  cube c(cube_path);
-
   grid g("../images/e1088_mag1_small");
+
+  std::cerr << g.cube_cache_size() << std::endl;
   return 0;
 }
 
