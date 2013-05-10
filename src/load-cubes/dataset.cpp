aab26ff2c7d21a2330d7a0535c0c207e28cb46da
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-10 16:02:30 +0200

    Fun with boost::filesystem. Trying to extract coordination data from dataset directory.

--- src/load-cubes/dataset.cpp
+++ src/load-cubes/dataset.cpp
@@ -9,6 +9,9 @@ dataset::dataset(const char* const dataset_dir)
   fs::recursive_directory_iterator file_itr(dataset_dir);
   for(; file_itr != end; ++file_itr) {
     if(!fs::is_directory(*file_itr) && fs::extension(*file_itr) == ".raw")
-      std::cerr << *file_itr << std::endl;
+      std::cerr << (*file_itr).path().filename().string() << std::endl;
   }
+
+  // todo: create a map from coord to files
+  // todo: find min and max x,y,z and store in dataset class member
 }
--- src/load-cubes/dataset.hpp
+++ src/load-cubes/dataset.hpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <boost/filesystem.hpp>
 
+#include "../load-cubes/coord.hpp"
+
 class dataset
 {
 public:
