ae12247df9b2d788cf0095873a540e7aeb4c718e
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-10 09:59:14 +0200

    Grid module stores cache of cubes.

--- src/load-cubes/grid.cpp
+++ src/load-cubes/grid.cpp
@@ -2,5 +2,41 @@
 
 grid::grid(const char* const dataset)
 {
-  std::cerr << "Loading grid from dataset." << std::endl;
+  std::cerr << "Loading grid from dataset " << dataset << "." << std::endl;
+
+  std::string cube_path = std::string(dataset) + "/x0005/y0007/z0007/070317_e1088_x0005_y0007_z0007.raw";
+
+  cube c(cube_path.c_str());
+  cube_cache.insert(std::make_pair(coord(1, 1, 1), c));
+}
+
+int grid::cube_cache_size() const
+{
+  return cube_cache.size();
+}
+
+bool coord::operator<(const coord& c) const
+{
+  if(x < c.x)
+    return true;
+  else if (c.x < x)
+    return false;
+  else //if(x == c.x)
+  {
+    if(y < c.y)
+      return true;
+    else if (c.y < y)
+      return false;
+    else //if(y == c.y)
+    {
+      if(z < c.z)
+	return true;
+      else if (c.z < z)
+	return false;
+      else //if(z == c.z)
+      {
+	return true;
+      }
+    }
+  }
 }
--- src/load-cubes/grid.hpp
+++ src/load-cubes/grid.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <map>
 
 #include "cube.hpp"
 
+struct coord
+{
+  int x, y, z;
+  coord(int X = -1, int Y = -1, int Z = -1)
+    : x(X), y(Y), z(Z) {}
+  bool operator<(const coord& c) const;
+};
+
 class grid
 {
+private:
+  std::map<coord, cube> cube_cache;
 public:
-grid(const char* const dataset);
+  grid(const char* const dataset);
+  int cube_cache_size() const;
 };
