786cb8349b70b64daf5f4ae22245c935d66ce457
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-11 10:34:09 +0200

    Rewrite of reading file bytes.

--- src/load-cubes/cube.cpp
+++ src/load-cubes/cube.cpp
@@ -16,20 +16,27 @@ cube::cube(const char* const filename)
   {
     data.reserve(dim * dim * dim);
 
-    std::copy(
-      std::istream_iterator<unsigned char>(cube_file),
-      std::istream_iterator<unsigned char>(),
-      std::back_inserter(data));
+    // There are some ugly bits below.
+    // First, we take raw pointer to vector: &data[0]
+    // Then, we need to read UNSIGNED chars, but we do not want
+    // to read signed chars and convert them to unsigned in following manner:
+    //
+    //    char c = ...;
+    //    unsigned char uc = c;
+    //
+    // Code above loses the sign bit.
+    //
+    // However, following code does the right thing, which means
+    // blindly copying sign bit as most significant bit of unsigned char.
+    //
+    //    char c = ...;
+    //    unsigned char uc = reinterpret_cast<char>(c);
+    //
+    // reinterpret_cast below is equivalent cast for arrays
 
-    if(data.size() != dim * dim * dim)
-    {
-      std::cerr
-	<< "Loading cube " << filename
-	<< " failed. Number of read bytes is " << data.size()
-	<< ", but should be " << dim * dim * dim << "."
-	<< std::endl;
-      exit(1);
-    }
+    cube_file.read(reinterpret_cast<char*>(&data[0]), dim * dim * dim);
+
+    assert(cube_file.good());
   }
 }
 
