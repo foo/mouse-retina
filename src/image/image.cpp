04b845a4cba6e0cd296c9a7040bf1e8d6479430c
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-11 15:08:05 +0200

    Basic implementation of image class.

--- src/image/image.cpp
+++ src/image/image.cpp
@@ -1,1 +1,31 @@
 #include "image.hpp"
+
+void image::white_rect(int w, int h)
+{
+  width = w;
+  height = h;
+
+  data.reserve(w * h);
+  std::fill_n(
+    std::back_inserter(data),
+    w * h,
+    static_cast<unsigned char>(255));
+}
+
+unsigned char& image::get(int x, int y)
+{
+  assert(0 <= x);
+  assert(x < width);
+  assert(0 <= y);
+  assert(y < height);
+  return data[x + width*y];
+}
+
+unsigned char image::get(int x, int y) const
+{
+  assert(0 <= x);
+  assert(x < width);
+  assert(0 <= y);
+  assert(y < height);
+  return data[x + width*y];
+}
--- src/image/image.hpp
+++ src/image/image.hpp
@@ -1,6 +1,16 @@
 #pragma once
 
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <cassert>
+
 class image
 {
 public:
+  void white_rect(int w, int h);
+  int width, height;
+  unsigned char& get(int x, int y);
+  unsigned char get(int x, int y) const;
+  std::vector<unsigned char> data;
 };
