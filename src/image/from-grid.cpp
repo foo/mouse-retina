5cecbf6ca50dd1b2fc6656fe674044d8a566c84f
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-11 15:29:40 +0200

    More assertions added to function that extracts image from grid.

--- src/image/from-grid.cpp
+++ src/image/from-grid.cpp
@@ -3,10 +3,15 @@
 image from_grid(grid& g, int x1, int x2, int y1, int y2, int z)
 {
   assert(x1 >= 0);
-  assert(x1 <= x2);
-
   assert(y1 >= 0);
+  assert(z  >= 0);
+
+  assert(x1 <= x2);
   assert(y1 <= y2);
-  
+
+  assert(x2 < g.size_x * cube::dim);
+  assert(y2 < g.size_y * cube::dim);
+  assert(z  < g.size_z * cube::dim);
+
   return image(); // equivalent to std::move(image)
 }
