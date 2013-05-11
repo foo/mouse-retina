d5039c4b7a965cf5495a3f1f2fb505755e9d685d
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-11 15:09:39 +0200

    Outputting white rectangle in PGM.

--- src/image-client/image-client.cpp
+++ src/image-client/image-client.cpp
@@ -3,8 +3,12 @@
 
 #include "../load-cubes/grid.hpp"
 #include "../image/image.hpp"
+#include "../image/pgm-export.hpp"
 
 int main(int argc, char* argv[])
 {
+  image i;
+  i.white_rect(20, 10);
+  pgm_export(i, boost::filesystem::path("../output/video/white_rect.pgm"));
   return 0;
 }
