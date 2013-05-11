0b78f1092b06c4e3f363c5e8c829bef43c0d6db9
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-11 17:45:56 +0200

    Create image from across three cubes.

--- src/image-client/image-client.cpp
+++ src/image-client/image-client.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
   const char* const dataset_dir = "../images/e1088_mag1_small";
   dataset d(dataset_dir);
   grid g(d);
-  image i = from_grid(g, 10, 200, 10, 200, 10);
+  image i = from_grid(g, 10, 300, 10, 300, 10);
   std::cerr << "Successfully loaded image from grid" << std::endl;
   
   pgm_export(i,
