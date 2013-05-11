fc5cf050d52fc4b8ead7c337d5c0c2cab2389055
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-11 17:04:44 +0200

    Order of parameters in image client fixed.

--- src/image-client/image-client.cpp
+++ src/image-client/image-client.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
   const char* const dataset_dir = "../images/e1088_mag1_small";
   dataset d(dataset_dir);
   grid g(d);
-  image i = from_grid(g, 10, 10, 200, 200, 10);
+  image i = from_grid(g, 10, 200, 10, 200, 10);
   
   pgm_export(i,
       boost::filesystem::path("../output/video/white_rect.pgm"));
