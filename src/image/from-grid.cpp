fb54ef7b974776e99c5b269c091bd783afd8154c
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-11 17:53:12 +0200

    Refactoring of from-grid code.

--- src/image/from-grid.cpp
+++ src/image/from-grid.cpp
@@ -8,7 +8,7 @@
   |         |        |         |        |        |   	 |     	  |  	 |
   +---------+--------+---------+--------+--------+-------+--------+------+
 
-  Rects symbolizes the grid, vertical line symbolizes selected row.
+  Rectangles symbolizes the grid, vertical line symbolizes selected row.
   2nd cube is left_cube, cubes 3-5 are interior cubes, 6th cube is right_cube.
  */
 
@@ -20,26 +20,28 @@ void copy_row(grid& g, int x1, int x2, int y, int z,
   const int x1_voxel = x1 % cube::dim;
   const int x2_voxel = x2 % cube::dim;
 
-  const int left_cube_number = x1 / cube::dim;
-  const int right_cube_number = x2 / cube::dim;
+  const int left_cube_x = g.min_x + x1 / cube::dim;
+  const int right_cube_x = g.min_x + x2 / cube::dim;
+  const int cube_y = g.min_y + y / cube::dim;
+  const int cube_z = g.min_z + z / cube::dim;
 
-  const cube& left_cube = g.get(g.min_x + left_cube_number, g.min_y + y / cube::dim, g.min_z + z / cube::dim);
+  const cube& left_cube = g.get(left_cube_x, cube_y, cube_z);
 
   img_iter = std::copy(
     left_cube.voxel_iter(x1_voxel, y_voxel, z_voxel),
     left_cube.voxel_iter(cube::dim, y_voxel, z_voxel),
     img_iter);
 
-  for(int c_x = left_cube_number + 1; c_x < right_cube_number; ++c_x)
+  for(int c_x = left_cube_x + 1; c_x < right_cube_x; ++c_x)
   {
-    const cube& interior_cube = g.get(g.min_x + c_x, g.min_y + y / cube::dim, g.min_z + z / cube::dim);
+    const cube& interior_cube = g.get(c_x, cube_y, cube_z);
     img_iter = std::copy(
       interior_cube.voxel_iter(0, y_voxel, z_voxel),
       interior_cube.voxel_iter(cube::dim, y_voxel, z_voxel),
       img_iter);
   }
   
-  const cube& right_cube = g.get(g.min_x + right_cube_number, g.min_y + y / cube::dim, g.min_z + z / cube::dim);
+  const cube& right_cube = g.get(right_cube_x, cube_y, cube_z);
 
   img_iter = std::copy(
     right_cube.voxel_iter(0, y_voxel, z_voxel),
