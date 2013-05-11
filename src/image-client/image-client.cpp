257b7da79c1e19d76f2ed0486237b1ceb48b3481
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-11 15:32:35 +0200

    Image client tests if modifying image works.

--- src/image-client/image-client.cpp
+++ src/image-client/image-client.cpp
@@ -9,6 +9,10 @@ int main(int argc, char* argv[])
 {
   image i;
   i.white_rect(20, 10);
+
+  for(int x = 5; x < 15; ++x)
+    i.get(x, 5) = 0;
+  
   pgm_export(i,
       boost::filesystem::path("../output/video/white_rect.pgm"));
   std::cerr << "Successfully wrote white rect." << std::endl;
