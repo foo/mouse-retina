02a114b2ab30c445e136caac92fce3197a9637e6
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-14 20:53:20 +0200

    Thresholding used in filters-client for edge detection.

--- src/filters-client/filters-client.cpp
+++ src/filters-client/filters-client.cpp
@@ -11,6 +11,7 @@
 #include "../filters/sobel.hpp"
 #include "../filters/gaussian.hpp"
 #include "../filters/sharpen.hpp"
+#include "../filters/threshold.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -21,65 +22,77 @@ int main(int argc, char* argv[])
   std::cerr << "Obtaining image from grid." << std::endl;
   image i = from_grid(g, 10, 300, 10, 300, 100);
 
-  std::cerr
-    << "Exporting original image to output/filters/original.pgm."
-    << std::endl;
+  {
+    std::cerr
+      << "Exporting original image to output/filters/original.pgm."
+      << std::endl;
   
-  pgm_export(i, boost::filesystem::path(
-      "../output/filters/original.pgm"));
+    pgm_export(i, boost::filesystem::path(
+	"../output/filters/original.pgm"));
+  }
 
-  std::cerr
-    << "Applying gaussian filter."
-    << std::endl;
+  {
+    std::cerr
+      << "Applying gaussian filter."
+      << std::endl;
   
-  image i_gaussian = gaussian5x5(i);
+    image i_gaussian = gaussian5x5(i);
   
-  std::cerr
-    << "Exporting image to output/filters/gaussian.pgm."
-    << std::endl;
+    std::cerr
+      << "Exporting image to output/filters/gaussian.pgm."
+      << std::endl;
   
-  pgm_export(i_gaussian, boost::filesystem::path(
-      "../output/filters/gaussian.pgm"));
-  
-  std::cerr
-    << "Applying sharpening filter."
-    << std::endl;
-  
-  image i_sharpen = sharpen(i);
-  
-  std::cerr
-    << "Exporting image to output/filters/sharpen.pgm."
-    << std::endl;
-  
-  pgm_export(i_sharpen, boost::filesystem::path(
-      "../output/filters/sharpen.pgm"));
-  
-  std::cerr
-    << "Applying sobel filter."
-    << std::endl;
-  
-  image i_sobel = sobel(i);
+    pgm_export(i_gaussian, boost::filesystem::path(
+	"../output/filters/gaussian.pgm"));
+  }
 
-  std::cerr
-    << "Exporting image to output/filters/sobel.pgm."
-    << std::endl;
+  {
+    std::cerr
+      << "Applying sharpening filter."
+      << std::endl;
   
-  pgm_export(i_sobel, boost::filesystem::path(
-      "../output/filters/sobel.pgm"));
+    image i_sharpen = sharpen(i);
   
-  std::cerr
-    << "Applying sobel filter to image after gaussian filter."
-    << std::endl;
+    std::cerr
+      << "Exporting image to output/filters/sharpen.pgm."
+      << std::endl;
+  
+    pgm_export(i_sharpen, boost::filesystem::path(
+	"../output/filters/sharpen.pgm"));
+  }
 
-  image i_gaussian_sobel = sobel(i_gaussian);
+  {
+    std::cerr
+      << "Applying sobel filter."
+      << std::endl;
   
-  std::cerr
-    << "Exporting image to output/filters/gaussian-sobel.pgm."
-    << std::endl;
+    image i_sobel = sobel(i);
+
+    std::cerr
+      << "Exporting image to output/filters/sobel.pgm."
+      << std::endl;
   
-  pgm_export(i_gaussian_sobel, boost::filesystem::path(
-      "../output/filters/gaussian-sobel.pgm"));
+    pgm_export(i_sobel, boost::filesystem::path(
+	"../output/filters/sobel.pgm"));
+  }
+
+  {
+    std::cerr
+      << "Applying all filters (edge detection)."
+      << std::endl;
+
+    image i_gaussian = gaussian5x5(i);
+    image i_gaussian_sharpened = sharpen(i_gaussian);
+    image i_thresholded = threshold(i_gaussian_sharpened, 160);
+    image i_edge_detection = sobel(i_thresholded);
   
+    std::cerr
+      << "Exporting image to output/filters/edge-detection.pgm."
+      << std::endl;
+  
+    pgm_export(i_edge_detection, boost::filesystem::path(
+	"../output/filters/edge-detection.pgm"));
+  }
 
   std::cerr << "Program finished successfully." << std::endl;
 
