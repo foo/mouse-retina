c18f6306a99fc678d8b19b5c0476b7b8296cbc99
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-14 20:56:18 +0200

    Do not use the same variable name as the function name, it encourages nasty errors to appear.

--- src/filters/threshold.cpp
+++ src/filters/threshold.cpp
@@ -1,10 +1,10 @@
 #include "threshold.hpp"
 
-void threshold_in_place(image& i, int threshold)
+void threshold_in_place(image& i, int threshold_value)
 {
   for(int x = 0; x < i.width(); ++x)
     for(int y = 0; y < i.height(); ++y)
-      if(i.get(x, y) < threshold)
+      if(i.get(x, y) < threshold_value)
 	i.get(x, y) = 0;
       else
 	i.get(x, y) = 255;
