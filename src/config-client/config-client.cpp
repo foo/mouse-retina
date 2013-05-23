066f24319ca83787db9db0865cc6878bf9b38370
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-23 15:18:01 +0200

    Config client sets up first config flag.

--- src/config-client/config-client.cpp
+++ src/config-client/config-client.cpp
@@ -2,11 +2,22 @@
 #include <cassert>
 
 #include "../config/knossos-config.hpp"
+#include "../config/options.hpp"
+
+namespace po = boost::program_options;
 
 int main(int argc, char* argv[])
 {
-  const char* const dataset_dir = "../images/e1088_mag1_small";
-  knossos_config c(dataset_dir);
+  po::options_description opt_desc("Available options");
+
+  opt_desc.add_options()
+    ("dataset-path,d",
+      po::value<std::string>()->default_value("../images/e1088_mag1_small"),
+      "Specify path to dataset in Knossos format.");
+
+  options opts(opt_desc, argc, argv);
+  
+  knossos_config c(opts.string_var("dataset-path"));
   c.print_config();
 
   return 0;
