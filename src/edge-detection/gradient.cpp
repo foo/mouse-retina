69bfef1a88b808e19af3432fce9545185ea53c32
Damian Rusak <dejmian.rusak@gmail.com>
2013-06-10 18:19:41 +0200

    structural changes to gradient class to boost readability and modularity of code

--- src/edge-detection/gradient.cpp
+++ src/edge-detection/gradient.cpp
@@ -11,47 +11,33 @@
 #include <sstream>
 #include <iomanip>
 
-Gradient::Gradient(int x_, int y_, double ang_, double mag_){
-	x = x_; y = y_; angle = ang_; mag = mag_;
-}
-
-double angle_from_two(double x, double y){
+double Gradient::angle_from_two(double x, double y){
 	if(iszero(x) && iszero(y)) return 0;
 	return atan2(y,x);
-    /*if(iszero(x)){
-        if(iszero(y)) return 0;
-        if(y > 0) return PI/2.;
-        else return 2*PI-PI/2.;
-    }
-    else if(x > 0) return atan2(y,x);
-    else if(iszero(y) || y > 0) return atan2(y,x)+PI;
-    else return atan2(y,x)-PI;*/
 }
 
-bool inline clos(double a, double b, double ep){
+bool inline Gradient::clos(double a, double b, double ep){
 	if(abs(a-b) < ep || abs(a-b-2*PI) < ep || abs(a-b+2*PI) < ep) return true;
 	return false;
 }
 
-bool inline corners_on_different_sides(int &Sx, int &Sy, double Gx, double Gy){
+bool inline Gradient::corners_on_different_sides(int &Sx, int &Sy, double Gx, double Gy){
 	//czy rogi kwadraciku o srodku w S sa po dwoch stronach wektora do G?
 	return (((Sx+0.5)*Gy - (Sy+0.5)*Gx) * ((Sx-0.5)*Gy - (Sy-0.5)*Gx) < 0 ||
 	   	    ((Sx+0.5)*Gy - (Sy-0.5)*Gx) * ((Sx-0.5)*Gy - (Sy+0.5)*Gx) < 0 );
 }
 
-int p[1000000], col[1000000];
-
-int Find(int x){
+int Gradient::Find(int x){
 	if(p[x] == x) return x;
 	p[x] = Find(p[x]);
 	return p[x];
 }
 
-void Union(int x, int y, Detector &det){
+void Gradient::Union(int x, int y){
 	if(Find(x) == Find(y)) return;
 	p[p[x]] = p[y];
-	det.G[x].push_back(y);
-	det.G[y].push_back(x);
+	detector.G[x].push_back(y);
+	detector.G[y].push_back(x);
 }
 
 inline int min(int a, int b){
@@ -62,49 +48,32 @@ inline int max(int a, int b){
 	if(a > b) return a; return b;
 }
 
-void join(int x1,int y1,int x2,int y2,image& out, int color){
+void Gradient::join(int x1,int y1,int x2,int y2,image& im, int color){
 	if(x1 > x2){ std::swap(x1,x2); std::swap(y1,y2);}
 	if(x1 != x2){
 		for(int x = 1; x < x2-x1; x++){
 			int y = (int)(y1 + x*(double)(y2-y1)/(double)(x2-x1));
-			if(out.pixel(x1+x,y)==0) out.pixel(x1+x,y) = color;
+			if(im.pixel(x1+x,y)==0) im.pixel(x1+x,y) = color;
 		}
 	}
 	else for(int y = min(y1,y2)+1; y < max(y1,y2); y++)
-			if(out.pixel(x1,y)==0) out.pixel(x1,y) = color;	
+			if(im.pixel(x1,y)==0) im.pixel(x1,y) = color;	
 }
 
-image gradient(const image& img1, int high_threshold, int low_threshold, int supp_radius, int kto){
-	int R = supp_radius;
-	int n = img1.width();
-	int m = img1.height();
+void Gradient::compute_gradients(){
+	LX = new double*[n];
+	LY = new double*[n];
+	mag = new double*[n];
+	ang = new double*[n];
+	supressed = new int*[n];
 	
-	double LX[n][m], LY[n][m], mag[n][m], ang[n][m];
-	int supressed[n][m];
-	
-	//to do zmiany, sciezka
-	FILE *params = fopen("../src/edge-detection/params.conf", "r");
-	char name[20]={0}; 
-	double ep1, ep2, ep3, sigma;
-	bool print_color, do_matching;
-	int union_ray, thresh_ray;
-	while(fscanf(params, "%s", name)!=EOF){
-		if(strcmp(name,"ep1")==0) fscanf(params,"%lf", &ep1);
-		if(strcmp(name,"ep2")==0) fscanf(params,"%lf", &ep2);
-		if(strcmp(name,"ep3")==0) fscanf(params,"%lf", &ep3);
-		if(strcmp(name,"print_color")==0) { int v; fscanf(params,"%d", &v); print_color=(bool)v;}
-		if(strcmp(name,"union_ray")==0) fscanf(params,"%d", &union_ray);
-		if(strcmp(name,"thresh_ray")==0) fscanf(params,"%d", &thresh_ray);
-		if(strcmp(name,"gaussian_sigma")==0) fscanf(params,"%lf", &sigma);
-		if(strcmp(name,"do_matching")==0) { int v; fscanf(params,"%d", &v); do_matching=(bool)v;}
+	for(int i = 0; i < n; i++){
+		LX[i] = new double[m];
+		LY[i] = new double[m];
+		mag[i] = new double[m];
+		ang[i] = new double[m];
+		supressed[i] = new int[m];
 	}
-	fclose(params);
-	
-	//image img = sharpen(gaussian(img1,sigma));
-	image img = gaussian(img1,sigma);
-	const char *pp = "../output/edge-detection/magnified.pgm";
-	if(!exists(boost::filesystem::path(pp)))	
-	pgm_export(img, boost::filesystem::path(pp));
 	
 	for(int i = 1; i < n-1; i++){
 		for(int j = 1; j < m-1; j++){
@@ -134,41 +103,14 @@ image gradient(const image& img1, int high_threshold, int low_threshold, int sup
 			//czy spelnione non-maximum suppression?
 		}
 	}
-	
-	image out(img);
+}
+
+void Gradient::suppress(){
 	std::stack<std::pair<int,int> >edges;
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < m; j++){
 			supressed[i][j] = 0;
-			
 			int I,J;
-			/*int x[2],y[2];
-			double a = ang[i][j]; if(a<0) a+=2*PI;
-			if		(a>=0 && a<PI/8)         {x[0]=1;y[0]=0;x[1]=-1;y[1]=0;}
-			else if (a>=PI/8 && a<PI/4+PI/8)     {x[0]=1;y[0]=1;x[1]=-1;y[1]=-1;}
-			else if (a>=PI/4+PI/8 && a<PI/2+PI/8){x[0]=0;y[0]=1;x[1]=0;y[1]=-1;}
-			else if (a>=PI/2+PI/8 && a<PI/2+PI/4+PI/8)  {x[0]=-1;y[0]=1;x[1]=1;y[1]=-1;}
-			else if (a>=PI/2+PI/4+PI/8 && a<PI+PI/8){x[0]=1;y[0]=0;x[1]=-1;y[1]=0;}
-			else if (a>=PI+PI/8 && a<PI+PI/4+PI/8){x[0]=1;y[0]=1;x[1]=-1;y[1]=-1;}
-			else if (a>=PI+PI/4+PI/8 && a<PI+PI/2+PI/8){x[0]=0;y[0]=1;x[1]=0;y[1]=-1;}
-			else if (a>=PI+PI/2+PI/8 && a<PI+PI/2+PI/4+PI/8){x[0]=-1;y[0]=1;x[1]=1;y[1]=-1;}
-			else if (a>=PI+PI/2+PI/4+PI/8 && a < 2*PI) {x[0]=1;y[0]=0;x[1]=-1;y[1]=0;}
-			
-			//for(int x = -R; x <= R; x++){
-			//	for(int y = -R; y <= R; y++){
-			for(int u = 0; u < 2; u++){
-					I = i+x[u]; J = j+y[u];
-					if(I < 0 || I >= n || J < 0 || J>=m) continue;
-					
-					if(corners_on_different_sides(x[u],y[u],LX[i][j],LY[i][j]) ||
-					   corners_on_different_sides(x[u],y[u],-LX[i][j],-LY[i][j])){
-						if(mag[I][J] > mag[i][j] && clos(ang[i][j],ang[I][J],ep1)){
-							supressed[i][j] = true;
-							break;
-						}
-					} 
-				
-			}*/
 			for(int x = -R; x <= R; x++){
 				for(int y = -R; y <= R; y++){
 					I = i+x; J = j+y;
@@ -188,9 +130,7 @@ image gradient(const image& img1, int high_threshold, int low_threshold, int sup
 				out.pixel(i,j) = 0;
 				continue;
 			}
-			//out.pixel(i,j) = mag[i][j]/2;
 			if(!supressed[i][j]){
-				//res.push_back(Gradient(i,j,ang[i][j],mag[i][j]));
 				out.pixel(i,j) = 100;
 				edges.push(std::make_pair(i,j));
 			} else out.pixel(i,j) = 0;
@@ -222,13 +162,16 @@ image gradient(const image& img1, int high_threshold, int low_threshold, int sup
 			}
 		}	
 	}
+}
+
+void Gradient::union_find_join(){
+	p = new int[n*m];
 	for(int i = 0; i < n*m; i++) p[i] = i;
 	//laczymy prostopadle
 	
-	Detector detector;
 	detector.G.resize(n*m);
 	
-	int clx, cly; int dist;
+	int clx, cly; int dist, I,J;
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < m; j++){
 			if(supressed[i][j]) continue;
@@ -246,25 +189,21 @@ image gradient(const image& img1, int high_threshold, int low_threshold, int sup
 				   	   dist = x*x+y*y; clx=I;cly=J;
 				   }
 				   if(clos(ang[i][j],ang[I][J],ep3)){
-					   Union(i*m+j, I*m+J, detector);
+					   Union(i*m+j, I*m+J);
 				   }
 				} 
 			}
 		}	
 		}
 	}
-	
-	int cc = 0;
-	int ile[n*m], scc=0;
-	srand(time(NULL));
-	int compM[n*m];
-	int father,fx,fy,ktos,y;
-	
-	image r,g,b;
+}
+
+void Gradient::assign_colors(){
 	if(print_color){
 		r = image(img);
 		g = image(img);
 		b = image(img);
+		col = new int[n*m];
 	}
 	
 	for(int i = 0; i < n*m; i++){
@@ -290,7 +229,58 @@ image gradient(const image& img1, int high_threshold, int low_threshold, int sup
 			compM[i] = -1;
 		}
 	}
-	std::vector<Compound>compound(scc);
+}
+
+void Gradient::read_params(){
+	//to do zmiany, sciezka
+	FILE *params = fopen("../src/edge-detection/params.conf", "r");
+	char name[20]={0}; 
+	double ep1, ep2, ep3, sigma;
+	bool print_color, do_matching;
+	int union_ray, thresh_ray;
+	while(fscanf(params, "%s", name)!=EOF){
+		if(strcmp(name,"ep1")==0) fscanf(params,"%lf", &ep1);
+		if(strcmp(name,"ep2")==0) fscanf(params,"%lf", &ep2);
+		if(strcmp(name,"ep3")==0) fscanf(params,"%lf", &ep3);
+		if(strcmp(name,"print_color")==0) { int v; fscanf(params,"%d", &v); print_color=(bool)v;}
+		if(strcmp(name,"union_ray")==0) fscanf(params,"%d", &union_ray);
+		if(strcmp(name,"thresh_ray")==0) fscanf(params,"%d", &thresh_ray);
+		if(strcmp(name,"gaussian_sigma")==0) fscanf(params,"%lf", &sigma);
+		if(strcmp(name,"do_matching")==0) { int v; fscanf(params,"%d", &v); do_matching=(bool)v;}
+	}
+	fclose(params);
+}
+
+image Gradient::gradient(const image& img1, int high_threshold, int low_threshold, int supp_radius, int kto){
+	
+	R = supp_radius;
+	n = img1.width();
+	m = img1.height();
+	this->high_threshold = high_threshold;
+	this->low_threshold = low_threshold;
+	
+	//image img = sharpen(gaussian(img1,sigma));
+	img = gaussian(img1,sigma);
+	const char *pp = "../output/edge-detection/magnified.pgm";
+	if(!exists(boost::filesystem::path(pp)))	
+	pgm_export(img, boost::filesystem::path(pp));
+	
+	out = image(img);
+	
+	compute_gradients();
+	suppress();
+	union_find_join();
+	
+	
+	int cc = 0;
+	int ile[n*m], scc=0;
+	srand(time(NULL));
+	compM = new int[n*m];
+	int father,fx,fy,ktos,y;
+	
+	
+	
+	compound.resize(scc);
 	std::vector<std::pair<int,int> >V;
 	for(int i = 0; i < n*m; i++){
 		ile[Find(i)]++;
@@ -346,7 +336,7 @@ image gradient(const image& img1, int high_threshold, int low_threshold, int sup
 	sprintf(sciezka, "../output/edge-detection/scc%d_%d_%d_%d_%d.ppm\n",
 			high_threshold, low_threshold, supp_radius, kto, 0);
 	if(print_color)
-		print_compounds(compound,0,sciezka,r,g,b,compM);
+		print_compounds(0,sciezka);
 	
 	int mode;
 	if(do_matching) mode = 0;
@@ -362,7 +352,7 @@ image gradient(const image& img1, int high_threshold, int low_threshold, int sup
 	sprintf(sciezka, "../output/edge-detection/scc%d_%d_%d_%d_%d.ppm\n",
 			high_threshold, low_threshold, supp_radius, kto, 1);
 	if(print_color)
-		print_compounds(compound,1,sciezka,r,g,b,compM);
+		print_compounds(1,sciezka);
 	
 	/*
 	for(int i = 0; i < n; i++){
@@ -397,8 +387,8 @@ image gradient(const image& img1, int high_threshold, int low_threshold, int sup
 	//return res;
 }
 
-void print_compounds(std::vector<Compound>&compound, int mode, char *path, image &r, image &g, image &b, int *compM){
-	
+void Gradient::print_compounds(int mode, char *path)
+{	
 	int n = r.width();
 	int m = r.height();
 	int fx,fy,ktos,y;
--- src/edge-detection/gradient.hpp
+++ src/edge-detection/gradient.hpp
@@ -14,11 +14,35 @@
 #define PI (2*acos(0))
 
 class Gradient{
-	int x, y; 
-	double angle, mag;
-	Gradient(int x_, int y_, double ang_, double mag_);
+	int x, y, n, m, R; 
+	double angle;
+	double ep1, ep2, ep3, sigma;
+	bool print_color, do_matching;
+	int union_ray, thresh_ray, high_threshold, low_threshold;
+	
+	int *p;, *col;
+	int *compM, **supressed;
+	double **LX, **LY, **mag, **ang;
+	std::vector<Compound>compound;
+	image img,out,r,g,b;
+	Detector detector;
+	
+	void read_params();
+	void compute_gradients(); 
+	void suppress();
+	void union_find_join();
+	void assign_colors();
+	
+	bool inline clos(double a, double b, double ep);
+	bool inline corners_on_different_sides(int &Sx, int &Sy, double Gx, double Gy);
+	double angle_from_two(double x, double y);
+	
+	int Find(int x);
+	void Union(int x, int y);
+	
+	void join(int x1,int y1,int x2,int y2,image& im, int color);
+	image gradient(const image& i, int high_threshold, int low_threshold, int supp_radius, int kto);
+	void print_compounds(int mode, char *path);
 };
 //vector<Gradient> gradient(const image& i);
-image gradient(const image& i, int high_threshold, int low_threshold, int supp_radius, int kto);
-double angle_from_two(double x, double y);
-void print_compounds(std::vector<Compound>&compounds,int mode, char *path, image &r, image &g, image &b,int*compM);
+
